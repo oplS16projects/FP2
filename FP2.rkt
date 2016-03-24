61b2bfddf772df9482a9b1e407c889858f3bb990
Aliyu Zakari <aliyu_zakari@student.uml.edu>
2016-03-23 22:52:15 -0400

    Updated Racket file
    I updated the source code.

--- FP2.rkt
+++ FP2.rkt
@@ -1,25 +1,5 @@
 #lang racket
 (require plot)
-;(plot-new-window? #t)
-
-;(plot (function sin (- pi) pi #:label "y = sin(x)"))
-
-;(parameterize ([plot-title  "An R × R → R function"]
-;                 [plot-x-label "x"]
-;                 [plot-y-label "y"]
-;                 [plot-z-label "cos(x) sin(y)"])
-;    (plot3d (contour-intervals3d (λ (x y) (* (cos x) (sin y)))
-;                                 (- pi) pi (- pi) pi)))
-
-;(plot (list (axes)
-;              (function sqr -2 2)
-;              (function (λ (x) x) #:color 0 #:style 'dot)
-;              (inverse sqr -2 2 #:color 3)))
-;
-;(plot (list (axes)
-;              (function sqr -2 2 #:label "y = x^2")
-;              (function (λ (x) x) #:label "y = x" #:color 0 #:style 'dot)
-;              (inverse sqr -2 2 #:label "x = y^2" #:color 3)))
 
 (define (make-square-graph x) (plot (function sqr (- x) x #:label "square of x")))
 (define (make-linear-graph y) (plot (function (λ (x) x) #:color 0 #:style 'dot)))
